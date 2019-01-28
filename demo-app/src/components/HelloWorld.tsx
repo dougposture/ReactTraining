57eb8b7728ba292376a421b2b975b7eac6497a63
Posture Interactive <posture@Dougs-MacBook-Pro.local>
2019-01-28 10:07:28 -0500

    CarTool beginning + HelloWorld

---
+++ demo-app/src/components/HelloWorld.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const HelloWorld = () => {
+    return <header>
+        <h1>Hello World!</h1>
+        <h2>Subtitle</h2>
+    </header>;
+};
---
+++ demo-app/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { CarTool } from './components/CarTool';
+
+ReactDOM.render(
+    <CarTool />,
+    document.querySelector('#root'),
+);
